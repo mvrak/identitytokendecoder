90f0978f2fce5fd9bfd3b91f67177c22463df3c8
Amit⚡ <amit@mvrak.com>
2020-11-02 05:43:49 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+const express = require("express");
+const msal = require('@azure/msal-node');
+const Mustache = require("mustache");
+var path = require('path');
+
+const SERVER_PORT = process.env.PORT || 3000;
+
+// Before running the sample, you will need to replace the values in the config, 
+// including the clientSecret
+const config = {
+    auth: {
+        clientId: "d4f423f2-cf21-480a-a338-2ac4d9c6191c",
+        authority: "https://login.microsoftonline.com/common",
+        clientSecret: "p-TCKbfJR-knV2~o69o3L1gR74l4QGC-c7"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
+};
+
+// Create msal application object
+const cca = new msal.ConfidentialClientApplication(config);
+
+// Create Express App and Routes
+const app = express();
+
+//set static directory for img/css/js
+app.use('/s/', express.static('public'));
+
+app.all("*", (req, res) => {	
+	res.sendFile(path.join(__dirname + '/t/index.html'));
+});
+
+app.get('/login', (req, res) => {
+const authCodeUrlParameters = {
+        scopes: ["user.read"],
+        redirectUri: "http://localhost:3000/redirect",
+    };
+
+    // get url to sign user in and consent to scopes needed for application
+    pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+        res.redirect(response);
+    }).catch((error) => console.log(JSON.stringify(error)));
+});
+
+
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        scopes: ["user.read"],
+        redirectUri: "http://localhost:3000/redirect",
+    };
+
+    pca.acquireTokenByCode(tokenRequest).then((response) => {
+        console.log("\nResponse: \n:", response);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+    });
+});
+
+
+app.listen(SERVER_PORT, () => console.log(`JWE Decoder server initialized on ${SERVER_PORT}!`))
