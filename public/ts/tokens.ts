d7f01645d4980754ff82e14fd8382c3344591dcc
Michael Manansala <mimanans@microsoft.com>
2020-11-02 13:29:25 -0800

    Decoded token to display claims

---
+++ public/ts/tokens.ts
@@ -0,0 +1,29 @@
+class Token {
+
+  public raw: string;
+  
+  constructor(token: string) {
+    this.raw = token;
+  }
+}
+
+// Pure JWTs
+class JWT extends Token {
+  public header: string;
+  public payload: string;
+  public signature: string;
+
+  constructor(token: string) {
+    super(token);
+
+    const parts = token.split(".");
+
+    if (parts.length != 3) {
+      throw new Error("Invalid token - JWTs have 3 parts separated by '.'");
+    }
+
+    this.header = atob(parts[0]);
+    this.payload = atob(parts[1]);
+    this.signature = parts[2];
+  }
+}
