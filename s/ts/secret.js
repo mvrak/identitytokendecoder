206a7d4e8e86c75b80bfa3a796e05971b8883a45
Michael Manansala <mimanans@microsoft.com>
2020-11-02 18:16:38 -0800

    Fixed ts compilation

--- s/ts/secret.js
+++
@@ -1,6 +0,0 @@
-var Secret = /** @class */ (function () {
-    function Secret() {
-    }
-    return Secret;
-}());
-//# sourceMappingURL=secret.js.map
--- s/ts/tokenModel.js
+++
@@ -1,7 +0,0 @@
-// Contains raw token string with metadata including title, 
-var TokenModel = /** @class */ (function () {
-    function TokenModel() {
-    }
-    return TokenModel;
-}());
-//# sourceMappingURL=tokenModel.js.map
