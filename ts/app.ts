92a9aa474bdb26867dd8d5e30c547fdd13bbf76a
Michael Manansala <mimanans@microsoft.com>
2020-11-03 09:16:25 -0800

    Fixed some spacing

--- ts/app.ts
+++ ts/app.ts
@@ -13,12 +13,12 @@ function w3_close() {
 function expandMenu(id: string) {
   let x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show"; 
-      x.previousElementSibling.className += " w3-red";
+    x.className += " w3-show"; 
+    x.previousElementSibling.className += " w3-red";
   } else { 
-      x.className = x.className.replace(" w3-show", "");
-      x.previousElementSibling.className = 
-      x.previousElementSibling.className.replace(" w3-red", "");
+    x.className = x.className.replace(" w3-show", "");
+    x.previousElementSibling.className = 
+    x.previousElementSibling.className.replace(" w3-red", "");
   }
 }
 
